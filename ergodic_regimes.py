8acac0101695537b825755d40c5c4b46092823ca
Sean Thoman (Owner/Founder) <114866071+regimelab@users.noreply.github.com>
2023-07-02 08:11:47 -0700

    Update ergodic_regimes.py

--- ergodic_regimes.py
+++ ergodic_regimes.py
@@ -1,14 +1,19 @@
 import numpy as np
+import pandas as pd 
 import matplotlib.pyplot as plt
+import gpytorch
+import torch 
+import seaborn as sns 
 
+from sklearn.cluster import KMeans
+
+    
 def generate_time_series(length, autocorr_decays):
     """
-    Generate a time series with long-range dependence.
-
+    Generate a time series with LRD using autocorr_decays array. 
     Parameters:
     - length: int, length of the time series
     - autocorr_decays: float array, decay factor controlling long-range dependence
-
     Returns:
     - time_series: numpy array, generated time series
     """
@@ -21,23 +26,20 @@ def generate_time_series(length, autocorr_decays):
 def calculate_auto_correlation(time_series):
     """
     Calculate auto-correlation of a time series.
-
     Parameters:
     - time_series: numpy array, input time series
-
     Returns:
     - auto_corr: numpy array, auto-correlation values
     """
     length = len(time_series)
     auto_corr = np.correlate(time_series, time_series, mode='full')
     auto_corr = auto_corr[length-1:]
-    auto_corr /= auto_corr[0]  # normalize by the first value
+    auto_corr /= auto_corr[0]  
     return auto_corr
 
 def plot_auto_correlation(auto_corr):
     """
     Plot the auto-correlation values.
-
     Parameters:
     - auto_corr: numpy array, auto-correlation values
     """
@@ -48,7 +50,7 @@ def plot_auto_correlation(auto_corr):
     plt.grid(True)
     plt.show()
 
-LENGTH = 120
+LENGTH = 100
 
 def compare_averages(time_series, window_size, autocorr_decays):
     """
@@ -76,14 +78,13 @@ def compare_averages(time_series, window_size, autocorr_decays):
 
 # Generate a time series with long-range dependence after wait period 
 rough_autocorr= [      0.9, 0.9, 0.9, 0.9, 0.9, 0.9,
-                       0.9, 0.9, 0.9, 0.9, 0.9, 0.9,
-                       0.6, 0.6, 0.6, 0.6,
-                       0.5, 0.4, 0.7, 0.7, 0.6,
-                       0.6, 0.6, 0.6, 0.6, 0.5, 0.4, 0.4,  
-                       0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 
-                       0.4, 0.4, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 
+                       0.6, 0.6, 0.6, 0.6, 0.5, 0.4, 
+                       0.7, 0.7, 0.6, 0.6, 0.6, 0.6, 
+                       0.6, 0.5, 0.4, 0.4, 0.4, 0.4, 
+                       0.4, 0.4, 0.3, 0.3, 0.3, 0.3, 
                        0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 
-                       0.3, 0.3, 0.3, 0.2, 0.1, 0.1 ]
+                       0.3, 0.3, 0.3, 0.3, 0.3, 0.2, 
+                       0.1, 0.1 ]
                        
 #rough_autocorr=[0]
 print(len(rough_autocorr))
@@ -96,7 +97,7 @@ auto_corr = calculate_auto_correlation(time_series)
 plot_auto_correlation(auto_corr[1:])
 
 # Compare ensemble average and time-domain average
-window_size = 40
+window_size = 10
 num_windows = None
 ensemble_avg = None
 time_domain_avg = None
@@ -115,59 +116,34 @@ axs[1].legend()
 plt.grid(True)
 plt.show()
 
-import gpytorch
-import torch 
-import seaborn as sns 
-from sklearn.cluster import KMeans
-
 for step in range(1, int(len(local_time_series) / 10)): 
         
-    # Evaluate kernel self similarity matrix (aka 'affinity matrix') 
+    # Evaluate kernel self similarity matrix (aka 'affinity matrix') as it grows with data 
     kernel = gpytorch.kernels.RBFKernel(lengthscale=10)
     C = (kernel(torch.tensor(local_time_series[:step*10])).evaluate()).detach().numpy() 
 
-
-    # Define the number of random Fourier features
-    #num_features = 3
-    # TODO possible to Generate random Fourier frequencies (random projection matrix)
-    #from sklearn.random_projection import GaussianRandomProjection
-    #random_projection = GaussianRandomProjection(n_components=num_features)
-    #random_projection.fit(C)
-
-    # Apply the random projection to the affinity matrix
-    #rff_approximation = random_projection.transform(C)
-    #sns.lineplot(data=rff_approximation)
-    #plt.show()
-    #print(rff_approximation)
-
-
     eigenvalues, eigenvectors = np.linalg.eig(C)
+    
     # Cluster eigenvalues (TODO Spectral clustering + Random Fourier Features + Wavelet connections)
     v0 = [float(x) for x in eigenvectors[:, 0]]
     v1 = [float(x) for x in eigenvectors[:, 1]]
-    v2 = [float(x) for x in eigenvectors[:, 2]]
-    import pandas as pd 
+
     featuredf = pd.DataFrame()
     featuredf['x0']=v0
     featuredf['x1']=v1
-    featuredf['x2']=v2
-    kmeans_n=3
+    kmeans_n=2
     kmeans_lbl = KMeans(n_clusters=kmeans_n).fit(featuredf).labels_
     fig,ax=plt.subplots()
-    #sns.scatterplot(data=v0,s=3.5,ax=ax)
-    #sns.scatterplot(data=v1,s=3.5,ax=ax)
-    #sns.scatterplot(data=v2,s=3.5,ax=ax)
 
-    sns.lineplot(data=local_time_series[:step*10], ax=ax)
+    sns.lineplot(data=np.cumsum(local_time_series[:step*10]), ax=ax)
     state_counts = np.zeros(kmeans_n)
     for M1 in kmeans_lbl:
         state_counts[M1] += 1 
     
     for M2 in range(len(kmeans_lbl)): 
-        if kmeans_lbl[M2] == np.argmax(state_counts):
+        if kmeans_lbl[M2] == np.argmin(state_counts):
             ax.axvline(M2, color='black', alpha=0.15)
     plt.show()
-
 
     # Plot the evaluation results
     fig, ax = plt.subplots()
@@ -185,4 +161,3 @@ for step in range(1, int(len(local_time_series) / 10)):
     # Show the plot
     plt.grid(False)
     plt.show()
-
